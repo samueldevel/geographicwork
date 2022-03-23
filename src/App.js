d5811dde6486490ee228c857cabed2905412ef58
Samuel Elias <samuel.resende@curva.com.br>
2022-03-22 23:11:43 -0300

    hell

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Checkbox } from "@mui/material";
+import {
+  BookmarkBorderIcon,
+  BookmarkIcon,
+  ThumbDownOffAlt,
+  ThumbDownAlt,
+} from "@mui/icons-material";
+
 function App() {
   return (
     <div>
       <h1>Bora dar uns Beijo?</h1>
+      <form>
+        <Checkbox
+          icon={<BookmarkBorderIcon />}
+          checkedIcon={<BookmarkIcon />}
+          name="check"
+        />
+        <Checkbox
+          icon={<ThumbDownOffAlt />}
+          checkedIcon={<ThumbDownAlt />}
+          name="check"
+        />
+      </form>
     </div>
   );
 }
