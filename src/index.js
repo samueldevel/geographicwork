caa1f156b68af529559e3b224b932b5e4570d395
Samuel Elias <samuel.resende@curva.com.br>
2022-03-27 20:57:47 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'react-slideshow-image/dist/styles.css';
+import './app.css';
 
 ReactDOM.render(
   <React.StrictMode>
