8983e9a0f6af177dee23b54f5f88b9b7e9c3b66e
Samuel Elias <samuel.resende@curva.com.br>
2022-03-27 19:02:30 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './app.css';
+import { Deck, Slide, Text } from 'spectacle';
+
+function App() {
+  return (
+    <Deck>
+      <Slide>
+        <Text>Hello</Text>
+      </Slide>
+      <Slide>
+        <Text>World</Text>
+      </Slide>
+    </Deck>
+  );
+}
+
+export default App;
