93545da3e7f3242a7f1808c394175c9f3d1f9f29
Samuel Elias <samuel.resende@curva.com.br>
2022-03-28 22:37:17 -0300

    te amo

--- src/Slides.jsx
+++ src/Slides.jsx
@@ -7,38 +7,39 @@ import {
   Box,
   FullScreen,
 } from 'spectacle';
-import index from './assets/odominiomorfoclimaticoamazonico-cke.webp';
-import vegetacao from './assets/vegetacao-da-regiao-norte-do-brasil-3.webp';
+import regiaoHidrografica from './assets/regiao_hidrográfica_amazônica.jpeg';
+import relevoImage from './assets/relevo_image.jpeg';
+import relevoTwoImage from './assets/relevo2_image.jpeg';
 
 export function FirstSlide() {
   return (
     <Slide>
       <FullScreen className="fullscreen" />
       <Heading fontSize={45} color="primary">
-        Domínio Morfoclimático da Amazônia
+        Hidrografia
       </Heading>
 
       <FlexBox justifyContent="space-between">
         <Box className="text-box">
-          <Text color="secondary">Introdução</Text>
-          <Text fontSize={20}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud <b>exercitation ullamco</b> laboris
-            nisi ut reprehenderit in voluptate velit esse cillum dolore eu
-            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in aliquip ex ea commodo consequat. Duis aute irure
-            dolor in culpa qui officia deserunt mollit anim id est laborum.
+          <Text></Text>
+          <Text fontSize={25}>
+            Por ser a maior bacia hidrográfica do Brasil e do mundo, a{' '}
+            <b>Bacia Amazônica</b> possui grande importância ambiental uma vez
+            que detém uma das maiores quantidades de água doce do planeta
           </Text>
-          <Text fontSize={18}>
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. <b> Excepteur</b> sint occaecat cupidatat non proident,
-            sunt in aliquip ex ea commodo consequat. Duis aute irure dolor in
+          <Text fontSize={25}>
+            A <b>Bacia Amazônica</b>, em conjunto com a Bacia do{' '}
+            <b>Tocantins-Araguaia </b>, forma o mais complexo sistema de água
+            doce do mundo.
           </Text>
         </Box>
 
-        <Image className="image" size="40%" height="70vh" src={index} />
+        <Image
+          className="image"
+          size="40%"
+          height="70vh"
+          src={regiaoHidrografica}
+        />
       </FlexBox>
     </Slide>
   );
@@ -47,40 +48,32 @@ export function FirstSlide() {
 export function SecondSlide() {
   return (
     <Slide>
-      <Heading fontSize={45} color="secondary">
-        Vegetação
+      <FullScreen className="fullscreen" />
+      <Heading fontSize={50} color="primary">
+        Relevo
       </Heading>
-
-      <FlexBox>
-        <Image className="image" size="40%" height="50vh" src={vegetacao} />
+      <FlexBox justifyContent="space-between">
         <Box className="text-box">
-          <Text className="text-box" fontSize={20}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod <b className="mark-bold">tempor incididunt ut</b> labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-            esse cillum dolore eu fugiat <b>nulla pariatur</b>. Excepteur sint
-            mollit anim id est laborum. occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt
-          </Text>
-          <Text fontSize={18}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          </Text>
+          <div className="fix-boxdiv">
+            <Text fontSize={25}>
+              Na Amazônia são encontradas três principais formas de relevo:
+              planícies, representadas pelas áreas inundadas pelos rios;
+              planaltos, representados pelas regiões de serras; e depressões,
+              como a região das depressões norte e sul amazônicas.
+            </Text>
+            <div id="relevo-two">
+              <Image
+                className="image"
+                size="90%"
+                height="30vh"
+                src={relevoTwoImage}
+              />
+            </div>
+          </div>
         </Box>
-      </FlexBox>
 
-      <Text fontSize={19}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod{' '}
-        a <b>nulla pariatur</b> liqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat <b>nulla pariatur</b>. Excepteur sint mollit anim id
-        est laborum. occaecat cupidatat non proident, sunt in culpa qui officia
-        deserunt
-      </Text>
+        <Image className="image" size="40%" height="70vh" src={relevoImage} />
+      </FlexBox>
     </Slide>
   );
 }
